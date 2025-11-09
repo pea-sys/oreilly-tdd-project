5f08141d290f8dd3db4f944c18b6aded1d44942f
pea-sys <pea98258@gmail.com>
2025-11-09 14:51:23 +0900

    feat: addition feature for Moneys in the same currency done

--- py/test_money.py
+++ py/test_money.py
@@ -1,5 +1,6 @@
 import unittest
-
+import functools
+import operator
 
 class Money:
   def __init__(self, amount, currency):
@@ -12,6 +13,18 @@ class Money:
 
   def divide(self, divisor):
     return Money(self.amount / divisor, self.currency)
+
+class Portfolio:
+  def __init__(self):
+    self.moneys = []
+  
+  def add(self, *moneys):
+    self.moneys.extend(moneys)
+
+  def evaluate(self, currency):
+    total = functools.reduce(
+    operator.add, map(lambda m: m.amount, self.moneys), 0)
+    return Money(total, currency)
 
 class TestMoney(unittest.TestCase):
   def testMultiplicationInDollars(self):
@@ -29,6 +42,14 @@ class TestMoney(unittest.TestCase):
     actualMoneyAfterDivision = originalMoney.divide(4)
     expectedMoneyAfterDivision = Money(1000.5, "KRW")
     self.assertEqual(expectedMoneyAfterDivision, actualMoneyAfterDivision)
+
+  def testAddition(self):
+    fiveDollars = Money(5, "USD")
+    tenDollars = Money(10, "USD")
+    fifteenDollars = Money(15, "USD")
+    portfolio = Portfolio()
+    portfolio.add(fiveDollars, tenDollars)
+    self.assertEqual(fifteenDollars, portfolio.evaluate("USD"))
 
 if __name__ == '__main__':
     unittest.main()
